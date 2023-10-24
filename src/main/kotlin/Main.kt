bb26bf9c4e9929087411b5e46f878f5ea8a50041
karina <135805670+karinakravets@users.noreply.github.com>
2023-10-24 22:44:14 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,22 @@
 fun main() {
-    println("Результат: " + chetvertaya(readln()))
+    var repeat = true
+    while (repeat) {
+        println("Введите числа и операцию : ")
+        val input = readLine()!!
+        val result = chetvertaya(input)
+        println("Результат: $result")
+        println("Хотите продолжить? (1 - да)")
+        val choice = readLine()!!.toInt()
+        repeat = (choice == 1)
+    }
+
 }
+
 fun chetvertaya(string: String): Float {
     var a = ""
     var b = ""
     var c = ""
     var d = true
-
     for (char in string.replace(" ", "")) {
         if (char.isDigit() || char == '.') {
             if (d) {
@@ -15,19 +25,17 @@ fun chetvertaya(string: String): Float {
                 b += char
             }
         } else if (char in setOf('+', '-', '*', '/')) {
-            c = char
+            c = char.toString()
             d = false
         }
     }
-
     val num1 = a.toFloat()
     val num2 = b.toFloat()
-
     return when (c) {
-        '+' -> num1 + num2
-        '-' -> num1 - num2
-        '*' -> num1 * num2
-        '/' -> num1 / num2
+        "+" -> num1 + num2
+        "-" -> num1 - num2
+        "*" -> num1 * num2
+        "/" -> num1 / num2
         else -> 0.0f
     }
 }
